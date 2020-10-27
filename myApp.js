fa86de401f1b1185bba449093faa2bbf4a16800e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-10-27 16:12:49 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -297,8 +297,15 @@ var removeManyPeople = function(done) {
 
 var queryChain = function(done) {
   var foodToSearch = "burrito";
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 1})
+    .limit(2)
+    .select({age: 0})
+    .exec(function(err, data) {
+      if (err) return console.log(err);
+      done(null, data);
+    });
   
-  done(null/*, data*/);
 };
 
 /** **Well Done !!**

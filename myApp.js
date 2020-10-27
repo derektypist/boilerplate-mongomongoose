6b4fe635931482b3f0a66cc87c67ff964d7c9bc9
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-10-27 15:22:07 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -155,8 +155,10 @@ var findPeopleByName = function(personName, done) {
 
 var findOneByFood = function(food, done) {
 
-  done(null/*, data*/);
-  
+  Person.findOne({favoriteFoods:food}, function(err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });  
 };
 
 /** 7) Use `Model.findById()` */
